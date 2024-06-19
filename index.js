9ee8cb2fab58af1c41e0c0acd65e000cee5988ba
Afia Rahman <rahmanmou26@gmail.com>
2024-06-19 18:26:08 +0600

    connected to database and created api to post user data

---
+++ index.js
@@ -0,0 +1,61 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: ["http://localhost:5173", "https://techtrove-auth.web.app", "https://techtrove-auth.firebaseapp.com"],
+    credentials: true
+}))
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctn12zm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db('techDB').collection('users');
+    const districtsCollection = client.db('techDB').collection('districts');
+
+    app.get('/districts', async(req, res) => {
+        const result = await districtsCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/users', async(req, res) => {
+        const userData = req.body;
+        const result = await userCollection.insertOne(userData)
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('TechTrove Server Running')
+})
+
+app.listen(port, () => {
+    console.log(`TechTrove Server Running on port ${port}`)
+})
